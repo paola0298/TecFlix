137e52dca35cbb08d523af4c2983150d6280523c
Paola <pvillegasch@gmail.com>
2019-09-23 22:49:42 -0600

    Changed initial app

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include "GUI/GUI.cpp"
-#include <gtkmm-3.0/gtkmm/application.h>
+// #include "GUI/GUI.cpp"
+// #include <gtkmm-3.0/gtkmm/application.h>
+#include "GUI/MainWindow.cpp"
 
 int main (int argc, char *argv[])
 {
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-    TecFlix gui;
-    gui.set_default_size(1000, 600);
+    MainWindow gui;
+    // gui.set_default_size(1000, 600);
     
 
     //Shows the window and returns when it is closed.
