765f2cf1c2996b0e2cf4f6993dc8e8dd4c1d0c96
Paola <pvillegasch@gmail.com>
2019-10-03 23:24:59 -0600

    Logic to reproduce trailer

--- src/GUI/TrailerWindow.hpp
+++ src/GUI/TrailerWindow.hpp
@@ -4,13 +4,14 @@
 #include <iostream>
 using namespace std;
 #include <gstreamer-1.0/gst/gst.h>
+#include <string.h>
 
 
 class TrailerWindow {
     public:
         TrailerWindow();
         ~TrailerWindow();
-        static void run(string url);
+        static int run(string url);
 
 };
 
